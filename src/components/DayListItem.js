bd9fb92250d8c430df6e79cf6d3cb8814fb96e28
Allen Ding <allen.ding@hotmail.com>
2023-02-21 21:26:08 -0800

    create DayListItem component and render stylings on storybook

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  // let spots = props.spots;
+
+  // const formatSpots = function() {
+  //   if (spots === 0) {
+  //     return 'no spots remaining'
+  //   }
+  //   if (spots === 1) {
+  //     return '1 spot remaining'
+  //   }
+  //   return `${spots} spots remaining`;
+  // }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
