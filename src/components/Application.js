91d4e617ac896b667a3fa590e0415f34a42b7723
Allen Ding <allen.ding@hotmail.com>
2023-02-22 17:00:39 -0800

    refactor

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -22,6 +23,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,8 +37,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
