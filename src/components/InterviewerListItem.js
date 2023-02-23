987af043fda79489b70e5bf3b7683876452d577e
Allen Ding <allen.ding@hotmail.com>
2023-02-22 18:15:27 -0800

    refactoring

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -14,7 +14,7 @@ export default function InterviewerListItem(props) {
 
   return (
     <li className={interviewerClass}
-      onClick={() => props.setInterviewer(props.id)}>
+      onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
