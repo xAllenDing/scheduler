633f0e17121882d53504d475ab27fcad6117d03b
Allen Ding <allen.ding@hotmail.com>
2023-02-23 15:32:40 -0800

    refactor for controlled components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
+import { useState } from 'react';
 
 export default function Form(props) {
+  console.log(props)
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function () {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = function () {
+    reset();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,17 +26,22 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.student}
+            value={student}
+            onChange={event => {
+              setStudent(event.target.value);
+            }}
           />
         </form>
+        
         <InterviewerList
           interviewers={props.interviewers}
-          value={props.interviewer}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
