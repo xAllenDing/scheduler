7ecc4e6719376a67458a1e4ccf214dd6dd147818
Allen Ding <allen.ding@hotmail.com>
2023-02-23 17:07:48 -0800

    add conditional rendering to index.js in Appointment

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -20,7 +20,7 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -32,7 +32,7 @@ export default function Form(props) {
             }}
           />
         </form>
-        
+
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty";
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+
+  const scheduled = function () {
+    return (props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>)
+  }
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {scheduled()}
+    </article>
   )
 }
