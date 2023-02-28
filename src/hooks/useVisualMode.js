1adbd9a45365475cb7e2785a0fb7b240da8bc37c
Allen Ding <allen.ding@hotmail.com>
2023-02-27 19:07:18 -0800

    add a hook and tests for the hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+
+export default function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode) {
+    setMode(newMode);
+  }
+
+  function back() {
+
+  }
+  return {mode, transition, back};
+}
+
