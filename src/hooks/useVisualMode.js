aea9e856cff48732de3dd5857b26a06a7c999e47
Allen Ding <allen.ding@hotmail.com>
2023-02-27 19:23:06 -0800

    complete the rest of the function and passed all tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,13 +5,26 @@ export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (newMode) {
+  function transition (newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    
     setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   }
 
   function back() {
+    if (history.length === 1) {
+      return;
+    }
+
+    history.pop();
+    const back = history[history.length - 1];
+    setMode(back);
 
   }
+
   return {mode, transition, back};
 }
 
